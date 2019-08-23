d486919d6aea07980f612a30992ab53d5b90c763
DerFrZocker <derrieple@gmail.com>
2019-08-23 19:59:14 +0200

    Impl BasicDao from SpigotUtils
    Took 13 minutes

--- api/src/main/java/de/derfrzocker/ore/control/api/dao/WorldOreConfigDao.java
+++ api/src/main/java/de/derfrzocker/ore/control/api/dao/WorldOreConfigDao.java
@@ -1,18 +1,8 @@
 package de.derfrzocker.ore.control.api.dao;
 
 import de.derfrzocker.ore.control.api.WorldOreConfig;
+import de.derfrzocker.spigot.utils.dao.BasicDao;
 
-import java.util.Optional;
-import java.util.Set;
-
-public interface WorldOreConfigDao {
-
-    Optional<WorldOreConfig> get(String name);
-
-    void remove(WorldOreConfig config);
-
-    void save(WorldOreConfig config);
-
-    Set<WorldOreConfig> getAll();
+public interface WorldOreConfigDao extends BasicDao<String, WorldOreConfig> {
 
 }
--- impl/yaml/src/main/java/de/derfrzocker/ore/control/impl/dao/WorldOreConfigYamlDao.java
+++ impl/yaml/src/main/java/de/derfrzocker/ore/control/impl/dao/WorldOreConfigYamlDao.java
@@ -1,45 +1,29 @@
 package de.derfrzocker.ore.control.impl.dao;
 
-import com.google.common.collect.Sets;
 import de.derfrzocker.ore.control.api.WorldOreConfig;
 import de.derfrzocker.ore.control.api.dao.WorldOreConfigDao;
 import de.derfrzocker.ore.control.impl.WorldOreConfigYamlImpl;
-import de.derfrzocker.spigot.utils.Config;
+import de.derfrzocker.spigot.utils.dao.yaml.BasicYamlDao;
 import lombok.NonNull;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 
-public class WorldOreConfigYamlDao implements WorldOreConfigDao {
+public class WorldOreConfigYamlDao extends BasicYamlDao<String, WorldOreConfig> implements WorldOreConfigDao {
 
-    @NonNull
-    private final File file;
-
-    private YamlConfiguration yaml; //TODO Check Thread safety
-
-    public WorldOreConfigYamlDao(final File file) {
-        this.file = file;
+    public WorldOreConfigYamlDao(File file) {
+        super(file);
     }
 
     @Override
     public Optional<WorldOreConfig> get(final @NonNull String name) {
-        return Optional.ofNullable(yaml.getSerializable(name, WorldOreConfigYamlImpl.class, null));
+        return getFromStringKey(name);
     }
 
     @Override
     public void remove(final @NonNull WorldOreConfig config) {
-        yaml.set(config.getName(), null);
-
-        try {
-            yaml.save(file);
-        } catch (IOException e) {
-            throw new RuntimeException("Unexpected error while save YamlConfiguration to file: " + file + ", while remove WorldOreConfig: " + config, e);
-        }
+        saveFromStringKey(config.getName(), null);
     }
 
     @Override
@@ -47,27 +31,7 @@ public class WorldOreConfigYamlDao implements WorldOreConfigDao {
         if (!(config instanceof ConfigurationSerializable))
             config = new WorldOreConfigYamlImpl(config.getName(), config.isTemplate(), config.getOreSettings(), config.getBiomeOreSettings());
 
-        yaml.set(config.getName(), config);
-
-        try {
-            yaml.save(file);
-        } catch (IOException e) {
-            throw new RuntimeException("Unexpected error while save YamlConfiguration to file: " + file + ", while save WorldOreConfig: " + config, e);
-        }
-    }
-
-    @Override
-    public Set<WorldOreConfig> getAll() {
-        return Sets.newHashSet(yaml.getKeys(false).stream().map(yaml::get).filter(Objects::nonNull).filter(value -> value instanceof WorldOreConfig).map(value -> (WorldOreConfig) value).toArray(WorldOreConfig[]::new));
-    }
-
-    public void init() {
-        yaml = new Config(file);
-        try {
-            yaml.save(file);
-        } catch (IOException e) {
-            throw new RuntimeException("Unexpected error while save YamlConfiguration to file: " + file, e);
-        }
+        saveFromStringKey(config.getName(), config);
     }
 
 }
