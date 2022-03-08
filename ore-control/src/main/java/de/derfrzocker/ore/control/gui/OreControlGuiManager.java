db85503d948fbe7617930b2158f2d6775a61a28e
DerFrZocker <derrieple@gmail.com>
2022-03-08 22:01:23 +0100

    Add below top offset integer type gui
    Took 4 minutes

--- ore-control/src/main/java/de/derfrzocker/ore/control/gui/OreControlGuiManager.java
+++ ore-control/src/main/java/de/derfrzocker/ore/control/gui/OreControlGuiManager.java
@@ -32,12 +32,14 @@ import de.derfrzocker.feature.common.value.number.integer.FixedDoubleToIntegerTy
 import de.derfrzocker.feature.common.value.number.integer.trapezoid.TrapezoidIntegerType;
 import de.derfrzocker.feature.common.value.number.integer.uniform.UniformIntegerType;
 import de.derfrzocker.feature.impl.v1_18_R2.value.offset.AboveBottomOffsetIntegerType;
+import de.derfrzocker.feature.impl.v1_18_R2.value.offset.BelowTopOffsetIntegerType;
 import de.derfrzocker.ore.control.api.OreControlManager;
 import de.derfrzocker.ore.control.gui.screen.ConfigInfoScreen;
 import de.derfrzocker.ore.control.gui.screen.ConfigInfosScreen;
 import de.derfrzocker.ore.control.gui.screen.FeatureSelectionScreen;
 import de.derfrzocker.ore.control.gui.screen.FeatureSettingsScreen;
 import de.derfrzocker.ore.control.gui.screen.value.AboveBottomOffsetIntegerScreen;
+import de.derfrzocker.ore.control.gui.screen.value.BelowTopOffsetIntegerScreen;
 import de.derfrzocker.ore.control.gui.screen.value.NumberEditScreen;
 import de.derfrzocker.ore.control.gui.screen.value.TrapezoidIntegerScreen;
 import de.derfrzocker.ore.control.gui.screen.value.UniformIntegerScreen;
@@ -86,6 +88,7 @@ public class OreControlGuiManager implements Listener {
         this.valueTypeInventoryGuis.put(UniformIntegerType.type(), UniformIntegerScreen.getGui(plugin, oreControlManager, this, settingFunction));
         this.valueTypeInventoryGuis.put(TrapezoidIntegerType.type(), TrapezoidIntegerScreen.getGui(plugin, oreControlManager, this, settingFunction));
         this.valueTypeInventoryGuis.put(AboveBottomOffsetIntegerType.type(), AboveBottomOffsetIntegerScreen.getGui(plugin, oreControlManager, this, settingFunction));
+        this.valueTypeInventoryGuis.put(BelowTopOffsetIntegerType.type(), BelowTopOffsetIntegerScreen.getGui(plugin, oreControlManager, this, settingFunction));
 
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
---
+++ ore-control/src/main/java/de/derfrzocker/ore/control/gui/screen/value/BelowTopOffsetIntegerScreen.java
@@ -0,0 +1,73 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 - 2021 Marvin (DerFrZocker)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package de.derfrzocker.ore.control.gui.screen.value;
+
+import de.derfrzocker.feature.impl.v1_18_R2.value.offset.AboveBottomOffsetIntegerValue;
+import de.derfrzocker.feature.impl.v1_18_R2.value.offset.BelowTopOffsetIntegerValue;
+import de.derfrzocker.ore.control.api.OreControlManager;
+import de.derfrzocker.ore.control.gui.OreControlGuiManager;
+import de.derfrzocker.ore.control.gui.PlayerGuiData;
+import de.derfrzocker.spigot.utils.guin.InventoryGui;
+import de.derfrzocker.spigot.utils.guin.builders.ButtonBuilder;
+import de.derfrzocker.spigot.utils.guin.builders.ButtonContextBuilder;
+import de.derfrzocker.spigot.utils.guin.builders.SingleInventoryGuiBuilder;
+import de.derfrzocker.spigot.utils.setting.ConfigSetting;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.util.function.Function;
+
+public class BelowTopOffsetIntegerScreen {
+
+    public static InventoryGui getGui(Plugin plugin, OreControlManager oreControlManager, OreControlGuiManager guiManager, Function<String, ConfigSetting> settingFunction) {
+        return SingleInventoryGuiBuilder
+                .builder()
+                .identifier("value.below_top_offset_integer_screen")
+                .withSetting(settingFunction.apply("design.yml"))
+                .withSetting(settingFunction.apply("feature_icons.yml"))
+                .withSetting(settingFunction.apply("value/below_top_offset_integer_screen.yml"))
+                .addButtonContext(ButtonContextBuilder
+                        .builder()
+                        .identifier("base")
+                        .button(ButtonBuilder
+                                .builder()
+                                .identifier("base")
+                                .withAction(clickAction -> clickAction.getClickEvent().setCancelled(true))
+                                .withAction(clickAction -> {
+                                    PlayerGuiData guiData = guiManager.getPlayerGuiData((Player) clickAction.getClickEvent().getWhoClicked());
+                                    if (!(guiData.getToEditValue() instanceof BelowTopOffsetIntegerValue value)) {
+                                        plugin.getLogger().warning(String.format("Expected a value of type '%s' but got one of type '%s', this is a bug!", BelowTopOffsetIntegerValue.class, guiData.getToEditValue() != null ? guiData.getToEditValue().getClass() : "null"));
+                                        return;
+                                    }
+
+                                    guiData.setToEditValue(value.getBase());
+
+                                    guiManager.openValueScreen((Player) clickAction.getClickEvent().getWhoClicked(), value.getBase());
+                                })
+                        )
+                ).build();
+    }
+}
