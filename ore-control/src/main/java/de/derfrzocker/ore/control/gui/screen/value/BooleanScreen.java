71a9d152404c2f017391dc61cd604bfaf964889a
DerFrZocker <derrieple@gmail.com>
2022-06-24 20:20:18 +0200

    Extract duplicated true / false code part in BooleanScreen
    Took 5 minutes

--- ore-control/src/main/java/de/derfrzocker/ore/control/gui/screen/value/BooleanScreen.java
+++ ore-control/src/main/java/de/derfrzocker/ore/control/gui/screen/value/BooleanScreen.java
@@ -31,7 +31,10 @@ import de.derfrzocker.ore.control.gui.PlayerGuiData;
 import de.derfrzocker.ore.control.gui.ScreenUtil;
 import de.derfrzocker.spigot.utils.gui.InventoryGui;
 import de.derfrzocker.spigot.utils.gui.builders.Builders;
+import de.derfrzocker.spigot.utils.gui.builders.ButtonContextBuilder;
 import org.bukkit.entity.Player;
+
+import java.util.Locale;
 
 public class BooleanScreen {
 
@@ -42,46 +45,31 @@ public class BooleanScreen {
                 .languageManager(guiValuesHolder.languageManager())
                 .withSetting(guiValuesHolder.settingFunction().apply("design.yml"))
                 .withSetting(guiValuesHolder.settingFunction().apply("value/boolean_screen.yml"))
-                .addButtonContext(Builders
-                        .buttonContext()
-                        .identifier("true")
-                        .button(Builders
-                                .button()
-                                .identifier("true")
-                                .withAction(clickAction -> clickAction.getClickEvent().setCancelled(true))
-                                .withAction(clickAction -> {
-                                    PlayerGuiData guiData = guiValuesHolder.guiManager().getPlayerGuiData(clickAction.getPlayer());
-                                    if (!(guiData.getToEditValue() instanceof FixedBooleanValue value)) {
-                                        guiValuesHolder.plugin().getLogger().warning(String.format("Expected a value of type '%s' but got one of type '%s', this is a bug!", FixedBooleanValue.class, guiData.getToEditValue() != null ? guiData.getToEditValue().getClass() : "null"));
-                                        return;
-                                    }
-
-                                    value.setValue(true);
-                                })
-                                .withAction(clickAction -> guiValuesHolder.guiManager().getPlayerGuiData(clickAction.getPlayer()).apply(guiValuesHolder.plugin(), guiValuesHolder.oreControlManager()))
-                        )
-                )
-                .addButtonContext(Builders
-                        .buttonContext()
-                        .identifier("false")
-                        .button(Builders
-                                .button()
-                                .identifier("false")
-                                .withAction(clickAction -> clickAction.getClickEvent().setCancelled(true))
-                                .withAction(clickAction -> {
-                                    PlayerGuiData guiData = guiValuesHolder.guiManager().getPlayerGuiData(clickAction.getPlayer());
-                                    if (!(guiData.getToEditValue() instanceof FixedBooleanValue value)) {
-                                        guiValuesHolder.plugin().getLogger().warning(String.format("Expected a value of type '%s' but got one of type '%s', this is a bug!", FixedBooleanValue.class, guiData.getToEditValue() != null ? guiData.getToEditValue().getClass() : "null"));
-                                        return;
-                                    }
-
-                                    value.setValue(false);
-                                })
-                                .withAction(clickAction -> guiValuesHolder.guiManager().getPlayerGuiData(clickAction.getPlayer()).apply(guiValuesHolder.plugin(), guiValuesHolder.oreControlManager()))
-                        )
-                )
+                .addButtonContext(getButton(guiValuesHolder, true))
+                .addButtonContext(getButton(guiValuesHolder, false))
                 .withBackAction((setting, guiInfo) -> guiValuesHolder.guiManager().getPlayerGuiData((Player) guiInfo.getEntity()).setPreviousToEditValue())
                 .addButtonContext(ScreenUtil.getBackButton(guiValuesHolder.guiManager()))
                 .build();
+    }
+
+    private static ButtonContextBuilder getButton(GuiValuesHolder guiValuesHolder, boolean newBoolValue) {
+        return Builders
+                .buttonContext()
+                .identifier(String.valueOf(newBoolValue).toLowerCase(Locale.ROOT))
+                .button(Builders
+                        .button()
+                        .identifier(String.valueOf(newBoolValue).toLowerCase(Locale.ROOT))
+                        .withAction(clickAction -> clickAction.getClickEvent().setCancelled(true))
+                        .withAction(clickAction -> {
+                            PlayerGuiData guiData = guiValuesHolder.guiManager().getPlayerGuiData(clickAction.getPlayer());
+                            if (!(guiData.getToEditValue() instanceof FixedBooleanValue value)) {
+                                guiValuesHolder.plugin().getLogger().warning(String.format("Expected a value of type '%s' but got one of type '%s', this is a bug!", FixedBooleanValue.class, guiData.getToEditValue() != null ? guiData.getToEditValue().getClass() : "null"));
+                                return;
+                            }
+
+                            value.setValue(newBoolValue);
+                        })
+                        .withAction(clickAction -> guiValuesHolder.guiManager().getPlayerGuiData(clickAction.getPlayer()).apply(guiValuesHolder.plugin(), guiValuesHolder.oreControlManager()))
+                );
     }
 }
