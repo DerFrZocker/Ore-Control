1049877fd0070e51bd29b2682416673bcbfea50a
DerFrZocker <git@derfrzocker.de>
2024-12-14 10:31:49 +0100

    feature(version): Add support for 1.21.4
    Took 39 minutes

--- api/src/main/java/de/derfrzocker/feature/api/Setting.java
+++ api/src/main/java/de/derfrzocker/feature/api/Setting.java
@@ -3,4 +3,5 @@ package de.derfrzocker.feature.api;
 import org.jetbrains.annotations.NotNull;
 
 public record Setting(@NotNull String name, @NotNull Class<?> valueType) {
+
 }
---
+++ impl/v1_21_R3/src/main/java/de/derfrzocker/feature/impl/v1_21_R3/value/heightmap/HeightmapType.java
@@ -0,0 +1,7 @@
+package de.derfrzocker.feature.impl.v1_21_R3.value.heightmap;
+
+import de.derfrzocker.feature.api.ValueType;
+import net.minecraft.world.level.levelgen.Heightmap;
+
+public abstract class HeightmapType implements ValueType<HeightmapValue, HeightmapType, Heightmap.Types> {
+}
