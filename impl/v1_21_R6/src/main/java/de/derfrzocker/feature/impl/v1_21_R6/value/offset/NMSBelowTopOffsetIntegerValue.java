06ad139010ec69c787afef0ccb2f52aae13561ce
DerFrZocker <git@derfrzocker.de>
2025-10-10 19:56:36 +0200

    feat: Add support for minecraft version 1.21.9 and 1.21.10
    Took 7 minutes

---
+++ impl/v1_21_R6/src/main/java/de/derfrzocker/feature/impl/v1_21_R6/value/offset/NMSBelowTopOffsetIntegerValue.java
@@ -0,0 +1,34 @@
+package de.derfrzocker.feature.impl.v1_21_R6.value.offset;
+
+import de.derfrzocker.feature.common.value.number.IntegerValue;
+import de.derfrzocker.feature.common.value.offset.BelowTopOffsetIntegerValue;
+import java.util.Random;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import org.bukkit.craftbukkit.v1_21_R6.generator.CraftLimitedRegion;
+import org.bukkit.generator.LimitedRegion;
+import org.bukkit.generator.WorldInfo;
+import org.bukkit.util.BlockVector;
+import org.jetbrains.annotations.NotNull;
+
+public class NMSBelowTopOffsetIntegerValue extends BelowTopOffsetIntegerValue {
+
+    public NMSBelowTopOffsetIntegerValue(IntegerValue base) {
+        super(base);
+    }
+
+    @Override
+    public Integer getValue(@NotNull WorldInfo worldInfo, @NotNull Random random, @NotNull BlockVector position, @NotNull LimitedRegion limitedRegion) {
+        int baseValue = getBase() == null ? 0 : getBase().getValue(worldInfo, random, position, limitedRegion);
+
+        WorldGenLevel level = ((CraftLimitedRegion) limitedRegion).getHandle();
+        ChunkGenerator chunkGenerator = level.getLevel().getChunkSource().getGenerator();
+
+        return Math.min(level.getHeight(), chunkGenerator.getGenDepth()) - 1 + Math.max(level.getMinY(), chunkGenerator.getMinY()) - baseValue;
+    }
+
+    @Override
+    public NMSBelowTopOffsetIntegerValue clone() {
+        return new NMSBelowTopOffsetIntegerValue(getBase() == null ? null : getBase().clone());
+    }
+}
--- ore-control/src/main/java/de/derfrzocker/ore/control/OreControl.java
+++ ore-control/src/main/java/de/derfrzocker/ore/control/OreControl.java
@@ -54,6 +54,7 @@ import de.derfrzocker.ore.control.impl.v1_21_R2.NMSReplacer_v1_21_R2;
 import de.derfrzocker.ore.control.impl.v1_21_R3.NMSReplacer_v1_21_R3;
 import de.derfrzocker.ore.control.impl.v1_21_R4.NMSReplacer_v1_21_R4;
 import de.derfrzocker.ore.control.impl.v1_21_R5.NMSReplacer_v1_21_R5;
+import de.derfrzocker.ore.control.impl.v1_21_R6.NMSReplacer_v1_21_R6;
 import de.derfrzocker.ore.control.interactions.BlockInteractionManager;
 import de.derfrzocker.spigot.utils.language.LanguageManager;
 import de.derfrzocker.spigot.utils.language.loader.FileLanguageLoader;
@@ -178,6 +179,8 @@ public class OreControl extends JavaPlugin implements Listener {
             return new NMSReplacer_v1_21_R4(this, oreControlManager, configParser);
         } else if (InternalVersion.v1_21_R5.getServerVersionRange().isInRange(version)) {
             return new NMSReplacer_v1_21_R5(this, oreControlManager, configParser);
+        } else if (InternalVersion.v1_21_R6.getServerVersionRange().isInRange(version)) {
+            return new NMSReplacer_v1_21_R6(this, oreControlManager, configParser);
         } else {
             throw new IllegalStateException(String.format("No NMSReplacer found for version '%s', this is a bug!", version));
         }
